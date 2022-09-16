608e8c72565ab4b181a70bed03945761ccf5dc69
kuropenguin <kuro1994.t@gmail.com>
2022-09-16 11:14:34 +0000

    add intergers iteration

---
+++ interges/adder_test.go
@@ -0,0 +1,22 @@
+package interges
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected '%d' bu got '%d'", expected, sum)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	//Output: 6
+
+}
